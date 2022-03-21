da71de1c2f3a7e530d5c309c0eba77c3da5e7e98
呱呱 <1034496147@qq.com>
2022-03-21 14:37:27 +0800

    Initial commit

---
+++ src/test/java/com/example/wlx_springboot/WlxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wlx_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlxSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
