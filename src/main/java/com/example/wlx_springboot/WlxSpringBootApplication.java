a94762a4fbeb5ee59af93ba6dacca215171e39b6
呱呱 <1034496147@qq.com>
2022-03-25 20:33:52 +0800

    修改

--- src/main/java/com/example/wlx_springboot/WlxSpringBootApplication.java
+++ src/main/java/com/example/wlx_springboot/WlxSpringBootApplication.java
@@ -1,8 +1,10 @@
 package com.example.wlx_springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class WlxSpringBootApplication {
